917368a9cab6ee0c49895f92555d985cff4eedf5
RavenCorpus <corp.rvn@gmail.com>
2024-02-15 18:48:32 -1000

    dsfadsofj

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rvncorpus/catkin_ws/src'
+whitelisted_packages = 'mobile_manipulator_body'.split(';') if 'mobile_manipulator_body' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rvncorpus/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/rvncorpus/catkin_ws/devel;/opt/ros/melodic' != '' else []
